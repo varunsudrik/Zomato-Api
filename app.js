3c1dbb49e52e2da581bf145290096e932de5e150
Varun Sudrik <varunsudrik99@gmail.com>
2023-04-17 15:03:20 +0530

    commit

--- app.js
+++ app.js
@@ -10,13 +10,21 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = process.env.PORT || 8700;
 const axios = require("axios");
-// var db;
 
-let authKey = "757ddd9a97d365906e7ad80e9dee2fee";
-
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+
+// key = locations
+// use header   'x-basic-token'
+var secKey = "91d03fd8a7a8c02ea93f50d31fa14132";
+function auth(key) {
+  if (key === secKey) {
+    return true;
+  }
+  return false;
+}
 
 // Conect DB
 mongoose
@@ -26,20 +34,73 @@ mongoose
     console.log(err);
   });
 
-// Create Schema
+// Create locationSchema
 let locationSchema = new mongoose.Schema({
-  location_id: Number,
-  location_name: String,
-  state_id: Number,
-  state: String,
-  country_name: String,
+  location_id: {
+    type: "Number",
+  },
+  location_name: {
+    type: "String",
+  },
+  state_id: {
+    type: "Number",
+  },
+  state: {
+    type: "String",
+  },
+  country_name: {
+    type: "String",
+  },
 });
 
-// key = locations
-var secKey = "91d03fd8a7a8c02ea93f50d31fa14132";
+// Create Restraunt Schema
+let restraundSchema = new mongoose.Schema({
+  restaurant_id: {
+    type: "Number",
+  },
+  restaurant_name: {
+    type: "String",
+  },
+  location_id: {
+    type: "Number",
+  },
+  state_id: {
+    type: "Number",
+  },
+  address: {
+    type: "String",
+  },
+  restaurant_thumb: {
+    type: "String",
+  },
+  average_rating: {
+    type: "Number",
+  },
+  rating_text: {
+    type: "String",
+  },
+  cost: {
+    type: "Number",
+  },
+  contact_number: {
+    type: "Number",
+  },
+  mealTypes: {
+    type: ["Mixed"],
+  },
+  cuisines: {
+    type: ["Mixed"],
+  },
+  image_gallery: {
+    type: ["String"],
+  },
+});
 
-// Create Collection
+// Create locationcollection
 const location = mongoose.model("location", locationSchema);
+
+// Create restrauntcollection
+const restraunt_data = mongoose.model("restraunt_data", restraundSchema);
 
 app.get("/", (req, res) => {
   res.send("Home Page");
@@ -47,14 +108,26 @@ app.get("/", (req, res) => {
 
 app.get("/locations", async (req, res) => {
   let key = req.header("x-basic-token");
-  if (key === secKey) {
+  if (auth(key)) {
     let result = await location.find();
     res.send(result);
   } else {
-    res.status(404).send("Unauthorised Access");
+    res.send("User unauthorised");
   }
 });
 
+app.get("/restraunts", async (req, res) => {
+  let key = req.header("x-basic-token");
+
+  if (auth(key)) {
+    let result = await restraunt_data.find();
+    res.send(result);
+  } else {
+    res.send("User unauthorised");
+  }
+});
+
+// Listener
 app.listen(4000, () => {
   console.log(`DB Connected at ${port}`);
 });
